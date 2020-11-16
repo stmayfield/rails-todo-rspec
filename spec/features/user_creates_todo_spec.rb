c784fd986197f15f4f9d384959079c826dc99ea6
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-11-16 12:01:23 -0800

    added sign_in support spec

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 feature "User creates todo" do
     scenario "successfully" do
-        visit root_path #Todos#index
+        # first pass: 'visit root_path' route, which is currently #Todos#index
+        sign_in
         click_on 'Add a new todo'
         fill_in 'Title', with: 'Buy milk'
         click_on 'Submit'
